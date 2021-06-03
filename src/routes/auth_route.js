0a2cae82d5c2a79a58555e8d5f33f2402f055a07
rajavasanthan <rajavasanthan@gmail.com>
2021-06-03 21:07:25 +0530

    secret

--- src/routes/auth_route.js
+++ src/routes/auth_route.js
@@ -27,13 +27,19 @@ passport.authenticate('facebook', { session: false, scope: ['public_profile'] })
 
 router.get('/facebook/token',async function(req,res){
     console.log(req.query.token)
-    let fbData = await fetch(`https://graph.facebook.com/me?access_token=${req.query.token}&fields=id,first_name,last_name,email`)
-    console.log("fbData")
+
+    let fbData = await fetch(`https://graph.facebook.com/oauth/access_token
+    ?client_id=1018964345278850
+    &client_secret=56559c918bdf34d8445c11a6cba767ab
+    &grant_type=client_credentials`)
+
+    // let fbData = await fetch(`https://graph.facebook.com/me?access_token=${req.query.token}&fields=id,first_name,last_name,email`)
+    // console.log("fbData")
     let data = await fbData.json()
     console.log(data)
-    res.json({
-        message : "Success"
-    })
+    // res.json({
+    //     message : "Success"
+    // })
 })
 
 router.get('/facebook/redirect',
